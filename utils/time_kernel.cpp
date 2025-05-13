5f17950945a69a43043f0b752d38719f4bc99c56
Abiel Almonte <almonteabiel@gmail.com>
2025-05-13 13:47:17 -0400

    refactor for stability

--- utils/time_kernel.cpp
+++ utils/time_kernel.cpp
@@ -1,159 +1,182 @@
-#include </opt/intel/oneapi/mkl/2025.1/include/mkl.h>
 #include <iostream>
 #include <cstdlib>
 #include <chrono>
+#include <cmath>
 #include <vector>
 #include <algorithm>
-#include <cstring>
-#include <cmath>
+
+#include </opt/intel/oneapi/mkl/2025.1/include/mkl.h>
+
+constexpr double TARGET_SEC= 0.5;
+constexpr uint32_t BASE_ITER= 500;
+constexpr uint32_t CACHE_LINE_SIZE= 64;
+constexpr uint32_t floats_per_cacheline= CACHE_LINE_SIZE/ sizeof(float); //16
 
 extern "C" void transpose_inplace_tiled_simd(float* A, const uint32_t m, const uint32_t stride);
 
-constexpr size_t CACHE_LINE_SIZE= 64;
-constexpr size_t floats_per_cacheline= CACHE_LINE_SIZE/ sizeof(float);
-
 inline size_t get_alloc_size(uint32_t n_floats){
-    size_t allocated_cache_lines= (n_floats + floats_per_cacheline - 1)/floats_per_cacheline;
-    return allocated_cache_lines* CACHE_LINE_SIZE;
+    uint32_t allocated_cache_lines= (n_floats + floats_per_cacheline - 1)/floats_per_cacheline;
+    return static_cast<size_t>(allocated_cache_lines* CACHE_LINE_SIZE);
 }
 
-static uint32_t autoTuneTimeIters(uint32_t m) {
-    using Clock = std::chrono::high_resolution_clock;
-    constexpr uint32_t WARMUP     = 10;
-    constexpr double   TARGET_SEC = 0.5;     // 100 ms target window
-    constexpr uint32_t MIN_ITERS  = 5;
-    constexpr uint32_t MAX_ITERS  = 1'000'000;
+static uint32_t get_iters(const uint32_t m, const uint32_t stride) {
+    std::vector<double> timings_s(BASE_ITER); //seconds
+    volatile double trash= 0;
 
-    uint32_t stride = m + floats_per_cacheline;
-    size_t total = size_t(m) * (stride);
-    float* A = static_cast<float*>(
-        aligned_alloc(64, get_alloc_size(total))
-    );
+    float* A= static_cast<float *>(aligned_alloc(CACHE_LINE_SIZE, get_alloc_size(m*stride)));
+
     if (!A) {
-        std::cerr << "autoTuneTimeIters: alloc failed\n";
-        std::exit(1);
+        std::cerr << "Aligned Alloc Failed" << std::endl;
+        std::exit(EXIT_FAILURE);
     }
-    for (size_t i = 0; i < m*m; ++i) {
-        A[i] = static_cast<float>(i);
+    
+    for (uint32_t i = 0; i < m; i++) {
+        for (uint32_t j= 0; j < m; j++){
+            A[i*stride + j] = static_cast<float>(i*m + j);
+        }
     }
 
-    for (uint32_t w = 0; w < WARMUP; ++w) {
+    for (uint32_t i = 0; i < 50; i++) {
         transpose_inplace_tiled_simd(A, m, stride);
     }
 
-    auto t0 = Clock::now();
-    transpose_inplace_tiled_simd(A, m, stride);
-    auto t1 = Clock::now();
+    for (uint32_t i = 0; i < BASE_ITER; i++){
+        auto start= std::chrono::high_resolution_clock::now();
+        transpose_inplace_tiled_simd(A, m, stride);
+        auto end= std::chrono::high_resolution_clock::now();
+
+        double latency_ns= std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+        timings_s[i]= latency_ns* 1e-9;
+
+        for (uint32_t ii = 0; ii < m; ii++) {
+            for (uint32_t jj= 0; jj < m; jj++){
+                trash+= A[ii*stride + jj];
+            }
+        }
+    }
+
     std::free(A);
-
-    double sec0 = std::chrono::duration<double>(t1 - t0).count();
-
-    uint32_t iters = uint32_t(std::ceil(TARGET_SEC / sec0));
-    iters = std::clamp(iters, MIN_ITERS, MAX_ITERS);
-    return iters;
+    
+    std::sort(timings_s.begin(), timings_s.end());
+    uint32_t p50_index= BASE_ITER*50u/100u;
+    double seconds_per_iteration= timings_s[p50_index];
+    
+    return static_cast<uint32_t>(std::ceil(TARGET_SEC/ seconds_per_iteration));
 }
 
-extern "C" int run_timing_benchmark(uint32_t m) {
-    uint32_t iters = autoTuneTimeIters(m);
-    const uint32_t warmup= 50;
-    const uint64_t bytes_transferred= static_cast<uint64_t>(m) * (m-1) / 2 * 16;
+extern "C" int run_timing_benchmark(const uint32_t m) {
+    const uint32_t stride= m + floats_per_cacheline;
+    const uint32_t iters= get_iters(m, stride);
+    const uint64_t bytes= static_cast<uint64_t>(((m * (m-1))/2)* sizeof(float) * 4); // 2 loads 2 stores
+    volatile double trash= 0;
     
-    uint32_t stride = m + floats_per_cacheline;
-    size_t total = size_t(m) * (stride);
-    float* A1= static_cast<float*>(aligned_alloc(64, get_alloc_size(total)));
-    float* A2= static_cast<float*>(aligned_alloc(64, get_alloc_size(total)));
+    float* A_custom= static_cast<float *>(aligned_alloc(CACHE_LINE_SIZE, get_alloc_size(m*stride)));
+    float* A_mkl= static_cast<float *>(aligned_alloc(CACHE_LINE_SIZE, get_alloc_size(m*stride)));
     
-    if (!A1 || !A2) {
-        std::cerr << "Memory allocation failed\n";
+    if (!A_custom || !A_mkl) {
+        std::cerr << "Aligned Alloc Failed" << std::endl;
         exit(EXIT_FAILURE);
     }
     
     for (uint32_t i= 0; i < m; i++) {
         for (uint32_t j= 0; j < m; j++) {
-            A1[i*stride + j]= A2[i*stride + j]= static_cast<float>(i*m + j);
+            float val=  static_cast<float>(i*m + j);
+
+            A_custom[i*stride + j]= val;
+            A_mkl[i*stride + j]= val;
         }
     }
     
-    std::vector<uint64_t> custom_times(iters);
-    std::vector<uint64_t> mkl_times(iters);
+    std::vector<double> custom_timings_ns(iters);
+    std::vector<double> mkl_timings_ns(iters);
     
-    for (uint32_t w= 0; w < warmup; ++w) {
-        transpose_inplace_tiled_simd(A1, m, stride);
-        mkl_simatcopy('R', 'T', m, m, 1.0f, A2, stride, stride);
+    for (uint32_t i= 0; i < 50; i++) {
+        transpose_inplace_tiled_simd(A_custom, m, stride);
+        mkl_simatcopy('R', 'T', m, m, 1.0f, A_mkl, stride, stride);
     }
     
     for (uint32_t i= 0; i < iters; i++) {
-        for (uint32_t r= 0; r < m; r++) {
-            for (uint32_t c= 0; c < m; c++) {
-                A1[r*stride + c]= static_cast<float>(r*m + c);
+        for (uint32_t ii= 0; ii < m; ii++) {
+            for (uint32_t jj= 0; jj < m; jj++) {
+                A_custom[ii*stride + jj]= static_cast<float>(ii*m + jj);
             }
         }
         
         auto start= std::chrono::high_resolution_clock::now();
-        transpose_inplace_tiled_simd(A1, m, stride);
+        transpose_inplace_tiled_simd(A_custom, m, stride);
         auto end= std::chrono::high_resolution_clock::now();
         
-        custom_times[i]= std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+        custom_timings_ns[i]= std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+        
+        for (uint32_t ii= 0; ii < m; ii++) {
+            for (uint32_t jj= 0; jj < m; jj++) {
+                trash+= A_custom[ii*stride + jj];
+            }
+        }
+        
     }
     
     for (uint32_t i= 0; i < iters; i++) {
-        for (uint32_t r= 0; r < m; r++) {
-            for (uint32_t c= 0; c < m; c++) {
-                A2[r*stride + c]= static_cast<float>(r*m + c);
+        for (uint32_t ii= 0; ii < m; ii++) {
+            for (uint32_t jj= 0; jj < m; jj++) {
+                A_mkl[ii*stride + jj]= static_cast<float>(ii*m + jj);
             }
         }
         
         auto start= std::chrono::high_resolution_clock::now();
-        mkl_simatcopy('R', 'T', m, m, 1.0f, A2, stride, stride);
+        mkl_simatcopy('R', 'T', m, m, 1.0f, A_mkl, stride, stride);
         auto end= std::chrono::high_resolution_clock::now();
         
-        mkl_times[i]= std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+        mkl_timings_ns[i]= std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
     }
     
-    std::sort(custom_times.begin(), custom_times.end());
-    std::sort(mkl_times.begin(), mkl_times.end());
-    
-    uint64_t custom_p10= custom_times[static_cast<size_t>(iters * 0.1)];
-    uint64_t custom_median= custom_times[iters/2];
-    uint64_t custom_p90= custom_times[static_cast<size_t>(iters * 0.90)];
-    
-    uint64_t mkl_p10= mkl_times[static_cast<size_t>(iters * 0.1)];
-    uint64_t mkl_median= mkl_times[iters/2];
-    uint64_t mkl_p90= mkl_times[static_cast<size_t>(iters * 0.90)];
-    
-    double custom_bw_p10= bytes_transferred / (double)custom_p10;  // GB/s
-    double custom_bw_median= bytes_transferred / (double)custom_median;
-    double custom_bw_p90= bytes_transferred / (double)custom_p90;
-
-    
-    double mkl_bw_p10= bytes_transferred / (double)mkl_p10;
-    double mkl_bw_median= bytes_transferred / (double)mkl_median;
-    double mkl_bw_p90= bytes_transferred / (double)mkl_p90;
-
-    
-    std::cout << "implementation\tmatrix_size\tbytes\tp10_ns\tmedian_ns\tp90_ns\tbw_p10_GB/s\tbw_median_GB/s\tbw_p90_GB/s\n";
-    std::cout << "custom\t" << m << "\t" << bytes_transferred << "\t" 
-              << custom_p10 << "\t" << custom_median << "\t" << custom_p90 << "\t"
-              << custom_bw_p10 << "\t" << custom_bw_median << "\t" << custom_bw_p90 << "\n";
-    
-    std::cout << "mkl\t" << m << "\t" << bytes_transferred << "\t" 
-              << mkl_p10 << "\t" << mkl_median << "\t" << mkl_p90 << "\t"
-              << mkl_bw_p10 << "\t" << mkl_bw_median << "\t" << mkl_bw_p90 << "\n";
-    
-    bool matrices_equal= true;
-    for (uint32_t r = 0; r < m; ++r){
-        for (uint32_t c = 0; c < m; ++c){
-            if (std::abs(A1[r*stride + c] - A2[r*stride + c]) > 1e-5)
-            matrices_equal = false;
+    //sanity check
+    for (uint32_t i = 0; i < m; i++){
+        for (uint32_t j = 0; j < m; j++){
+            float got= A_custom[i*stride + j];
+            float want= A_mkl[i*stride + j];
+            
+            if (std::abs(got - want) > 1e-5f){
+                std::cerr << "Sanity Check Failed: want " << want << " got " << got << std::endl;
+                exit(EXIT_FAILURE);
+            }
         }
     }
     
-    if (!matrices_equal) {
-        std::cerr << "WARNING: Matrix results differ between implementations\n";
-    }
+    std::free(A_custom);
+    std::free(A_mkl);
     
-    free(A1);
-    free(A2);
+    std::sort(custom_timings_ns.begin(), custom_timings_ns.end());
+    std::sort(mkl_timings_ns.begin(), mkl_timings_ns.end());
+    
+    uint32_t p10_index= iters*10u/100u;
+    uint32_t p50_index= iters*50u/100u;
+    uint32_t p90_index= iters*90u/100u;
+
+    double custom_p10_latency_ns= custom_timings_ns[p10_index]; //best case sustained, smaller
+    double custom_p50_latency_ns= custom_timings_ns[p50_index];
+    double custom_p90_latency_ns= custom_timings_ns[p90_index]; //worst case sustained, larger
+
+    double mkl_p10_latency_ns= mkl_timings_ns[p10_index];
+    double mkl_p50_latency_ns= mkl_timings_ns[p50_index];
+    double mkl_p90_latency_ns= mkl_timings_ns[p90_index];
+    
+    double custom_p10_bandwidth= static_cast<double>(bytes/(custom_p10_latency_ns)); //best case sustained, larger
+    double custom_p50_bandwidth= static_cast<double>(bytes/(custom_p50_latency_ns));
+    double custom_p90_bandwidth= static_cast<double>(bytes/(custom_p90_latency_ns)); //worst cast sustained, smaller
+    
+    double mkl_p10_bandwidth= static_cast<double>(bytes/(mkl_p10_latency_ns));
+    double mkl_p50_bandwidth= static_cast<double>(bytes/(mkl_p50_latency_ns));
+    double mkl_p90_bandwidth= static_cast<double>(bytes/(mkl_p90_latency_ns));
+
+    std::cout << "implemenation" << "\t" << "size"  << "\t" << "p10_latency_ns" << "\t" << "p50_latency_ns" << "\t" <<"p90_latency_ns";
+    std::cout <<  "\t" << "p10_bandwidth" << "\t" << "p50_bandwidth" << "\t" << "p90_bandwidth"  << std::endl;
+
+    std::cout << "custom" << "\t" << m << "\t" << custom_p10_latency_ns << "\t" << custom_p50_latency_ns << "\t" << custom_p90_latency_ns;
+    std::cout << "\t" << custom_p10_bandwidth << "\t" << custom_p50_bandwidth << "\t" << custom_p90_bandwidth << std::endl;
+
+    std::cout << "mkl" << "\t" << m << "\t" << mkl_p10_latency_ns << "\t" << mkl_p50_latency_ns << "\t" << mkl_p90_latency_ns;
+    std::cout << "\t" << mkl_p10_bandwidth << "\t" << mkl_p50_bandwidth << "\t" << mkl_p90_bandwidth << std::endl;
     
     return 0;
 }
