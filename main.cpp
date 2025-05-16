26f2c09a4f6c7025f769e3d3cf4e9262022f68cd
abiel almonte <almonteabiel@gmail.com>
2025-05-16 15:55:29 -0400

    check for linux

--- main.cpp
+++ main.cpp
@@ -3,22 +3,24 @@
 #include <cstdlib>
 #include <cstdint>
 
-extern "C" {
-    int run_performance_counters(const uint32_t m, const float alpha);
-    int run_timing_benchmark(const uint32_t m, const float alpha);
-}
+extern "C" int run_timing_benchmark(const uint32_t m, const float alpha);
 
-void pin_to_cpu(int cpu_id) {
-    cpu_set_t cpu_set;
-    CPU_ZERO(&cpu_set);
-    CPU_SET(cpu_id, &cpu_set);
-    int rv= sched_setaffinity(0, sizeof(cpu_set_t), &cpu_set);
-    if(rv < 0){
-        std::cout << "Failed to pin to cpu" << std::endl;
-        exit(EXIT_FAILURE);
+#if defined(__linux__)
+
+    extern "C" int run_performance_counters(const uint32_t m, const float alpha);
+
+    void pin_to_cpu(int cpu_id) {
+        cpu_set_t cpu_set;
+        CPU_ZERO(&cpu_set);
+        CPU_SET(cpu_id, &cpu_set);
+        int rv= sched_setaffinity(0, sizeof(cpu_set_t), &cpu_set);
+        if(rv < 0){
+            std::cout << "Failed to pin to cpu" << std::endl;
+            exit(EXIT_FAILURE);
+        }
     }
-}
-
+    
+#endif
 
 int main(int argc, char** argv) {
     if (argc < 4) {
@@ -29,14 +31,18 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
     
-    pin_to_cpu(0);
+    #if defined(__linux__)
+        pin_to_cpu(0);
+    #endif
 
     std::string mode= argv[1];
     uint32_t matrix_size= std::atoi(argv[2]);
     float alpha= std::atof(argv[3]);
 
     if (mode == "perf") {
-        return run_performance_counters(matrix_size, alpha);
+        #if defined(__linux__)
+            return run_performance_counters(matrix_size, alpha);
+        #endif
     } 
     else if (mode == "time") {
         return run_timing_benchmark(matrix_size, alpha);
